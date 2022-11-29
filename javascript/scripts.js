7c96486f8007138d134d5999042055e9aa471f69
yuh <yurims.mw@gmail.com>
2022-11-29 12:00:07 -0300

    master

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,3 +1,38 @@
-function main(){
-    
+async function mapImageList(){
+    const memesObject = [
+        {"name": "chapolin",
+        "path": "pictures/chapolin.jpg"},
+
+        {"name": "chloe",
+        "path": "pictures/chloe.jpg"},
+
+        {"name": "funny-cat1",
+        "path": "pictures/funny-cat1.jpg"},
+
+        {"name": "funny-cat2",
+        "path": "pictures/funny-cat2.jpg"},
+    ]
+
+    return memesObject
+
 }
+
+async function createGallery(imageList){
+    const memesSelector = document.querySelector("#memesList")
+
+    imageList.forEach(picture => {
+        let newOption = document.createElement("option")
+        newOption.text = picture.name.toUpperCase()
+        newOption.value = picture.path
+        memesSelector.appendChild(newOption)
+    });
+
+}
+
+function main(){
+    const memesImageList = await mapImageList()
+    await createGallery(memesImageList)
+
+}
+
+main()
