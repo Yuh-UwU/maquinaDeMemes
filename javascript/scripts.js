ad8cd841a0a5163b4f7a4c1a7fa97c672ce84712
yuh <yurims.mw@gmail.com>
2022-11-29 15:29:03 -0300

    master

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,61 +1,72 @@
-function enablePhotoUpload() {
+function enablePhotoUpload(){
     const imageInput = document.querySelector("#imageInput")
-
+  
     imageInput.addEventListener("change", function(){
-        const reader = new FileReader()
-
-        reader.addEventListener("load", ()=>{
-            const uploadImage = reader.result
-
-            changeMemePicture(uploadImage)
-        })
-        reader.readAsDataURL(this.files[0])
+      const reader = new FileReader()
+  
+      reader.addEventListener("load", ()=>{
+        const uploadImage = reader.result
+  
+        changeMemePicture(uploadImage)
+        
+      })
+  
+      reader.readAsDataURL(this.files[0])
+  
     })
-}
-
-async function mapImageList(){
+  
+  }
+  
+  async function mapImageList(){
     const memesObject = [
-        {"name": "chapolin",
-        "path": "pictures/chapolin.jpg"},
-
-        {"name": "chloe",
-        "path": "pictures/chloe.jpg"},
-
-        {"name": "funny-cat 1",
-        "path": "pictures/funny-cat1.png"},
-
-        {"name": "funny-cat 2",
-        "path": "pictures/funny-cat2.png"},
+      {
+        "name": "chapolin",
+        "path": "pictures/chapolin.jpg"
+      },
+      {
+        "name": "chloe",
+        "path": "pictures/chloe.jpg"
+      },
+      {
+        "name": "funny-cat 1",
+        "path": "pictures/funny-cat1.png"
+      },
+      {
+        "name": "funny-cat 2",
+        "path": "pictures/funny-cat2.png"
+      },
+  
     ]
-
+  
     return memesObject
-
-}
-
-async function createGallery(imageList){
-    const memesSelector = document.querySelector("#memeList")
-
+  
+  }
+  
+  async function createGallery(imageList){
+    const memeSelector = document.querySelector("#memeList")
+  
     imageList.forEach(picture => {
         let newOption = document.createElement("option")
         newOption.text = picture.name.toUpperCase()
         newOption.value = picture.path
-        memesSelector.appendChild(newOption)
+        memeSelector.appendChild(newOption)
     });
-
-}
-
-async function changeMemePicture(photo){
+  }
+  
+  
+  async function changeMemePicture(photo) {
     let displayImage = document.querySelector("#displayImage")
     displayImage.style.backgroundImage = `url('${photo}')`
-    
-}
-
-async function main(){
+  }
+  
+  
+  async function main(){
     const memesImageList = await mapImageList()
+    
     enablePhotoUpload()
     await createGallery(memesImageList)
     await changeMemePicture(memesImageList[1].path)
-
-}
-
-main()
+    
+  }
+  
+  main()
